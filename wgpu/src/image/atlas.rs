b0db8f9b1df3ef3b783bd2e7472676248eaae502
RabbyDevs <67389402+RabbyDevs@users.noreply.github.com>
2025-05-11 19:36:54 +0300

    remove unnecessary atlasing

--- wgpu/src/image/atlas.rs
+++ wgpu/src/image/atlas.rs
@@ -15,14 +15,12 @@ pub const SIZE: u32 = 2048;
 use crate::core::Size;
 use crate::graphics::color;
 
-use std::sync::Arc;
-
 #[derive(Debug)]
 pub struct Atlas {
     texture: wgpu::Texture,
     texture_view: wgpu::TextureView,
     texture_bind_group: wgpu::BindGroup,
-    texture_layout: Arc<wgpu::BindGroupLayout>,
+    texture_layout: wgpu::BindGroupLayout,
     layers: Vec<Layer>,
 }
 
@@ -30,7 +28,7 @@ impl Atlas {
     pub fn new(
         device: &wgpu::Device,
         backend: wgpu::Backend,
-        texture_layout: Arc<wgpu::BindGroupLayout>,
+        texture_layout: wgpu::BindGroupLayout,
     ) -> Self {
         let layers = match backend {
             // On the GL backend we start with 2 layers, to help wgpu figure
